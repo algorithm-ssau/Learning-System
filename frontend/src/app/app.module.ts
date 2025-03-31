9777ef76132d147953a8cba0afd0b6e6dc82c85d
Daytel <159631907+Daytel@users.noreply.github.com>
2025-03-31 13:58:21 +0400

    Add page evaluation task

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,17 +8,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { GameStateService } from 'src/services/game-state.service';
 import { AppComponent } from './app.component';
 import { FieldEditorComponent } from 'src/pages/field-editor/field-editor.component';
+import { TaskViewComponent } from 'src/pages/task-view/task-view.component';
 
 const roots: Routes = [
   {path: '', component: AppComponent}, // Заменить авторизацией
   {path: 'editor', component: FieldEditorComponent},
+  {path: 'view', component: TaskViewComponent},
   {path: '**', component: AppComponent}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    FieldEditorComponent
+    FieldEditorComponent,
+    TaskViewComponent
   ],
   imports: [
     BrowserModule,
