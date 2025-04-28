f2f070f7c629b0f73f66be71cc9cae81705f43dc
Nikolay Samokhvalov <bombdaytel@gmail.com>
2025-04-29 01:58:28 +0400

    Refining the logic of the field editor

--- frontend/src/services/game-state.service.ts
+++ frontend/src/services/game-state.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 export interface GameField {
+    id?: number;
     width: number,
     height: number,
     energy: number,
-    gameField: number[]
+    gameField: number[],
+    collectCoins: boolean;
 }
+
+export interface GameElement {
+    name: string;
+    image: string;
+    count: number;
+    max: number;
+    index: number;
+}
+
 export interface Task {
     name: string,
     text: string,
@@ -18,21 +30,45 @@ export interface Task {
 })
 
 export class GameStateService {
-    gf: GameField;
+    private gfSubject: BehaviorSubject<GameField>; // Храним состояние поля через BehaviorSubject
+    width = 10;
+    height = 10;
+    ge: GameElement[] = [
+        { name: 'Empty', image: 'assets/empty.png', count: 0, max: this.width * this.height, index: 0},
+        { name: 'Rock', image: 'assets/rock.png', count: 0, max: 30, index: 1 },
+        { name: 'Coin', image: 'assets/coin.png', count: 0, max: 10, index: 2 },
+        { name: 'Hole', image: 'assets/hole.png', count: 0, max: 20, index: 3 },
+        { name: 'Ant', image: 'assets/ant.png', count: 0, max: 1, index: 4 }
+      ];
 
     constructor(){
-        this.gf = {width: 10, height: 10, gameField: Array(100).fill(0), energy: 10};
+        const initialField: GameField = {
+            width: this.width,
+            height: this.height,
+            gameField: Array(this.width * this.height).fill(0),
+            energy: 10,
+            collectCoins: false
+        };
+      
+        this.gfSubject = new BehaviorSubject<GameField>(initialField);
     }
 
+    // Установить новое игровое поле
     setGameField(gameField: GameField) {
-        this.gf = gameField;
+        this.gfSubject.next(gameField); // обновляем через next
     }
-    
-    getGameField(): GameField {
-        return this.gf;
+
+    // Получить Observable поля
+    getGameField(): Observable<GameField> {
+        return this.gfSubject.asObservable();
     }
 
     saveTask(task: Task){
         console.log(task);
+    }
+
+    // Получить элементы
+    getGameElements(): GameElement[] {
+        return this.ge;
     }
 }
